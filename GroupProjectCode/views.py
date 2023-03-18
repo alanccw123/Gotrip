756c40563da903688feeebb4ab75847a9d7a0c1a
Ryan <284236428@qq.com>
2023-03-18 17:58:46 +0000

    comment

--- GroupProjectCode/views.py
+++ GroupProjectCode/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-
-from django.http import HttpResponse
-
 from django.http import HttpResponse
 
 from GroupProjectCode import models
@@ -19,14 +16,14 @@ def index(request):
 def manage_addtrips(request):
     if request.method == "GET":
         return render(request, "manage_addtrips.html")
-    # 获取用户提交的数据
+    # Get user submitted data
     # Trip_id = request.POST.get("Trip_id")
     tripname = request.POST.get("tripname")
     introduction = request.POST.get("introduction")
     image_url = request.FILES.get("image_url")
     # booked_num = request.POST.get("booked_num")
     Trip_price = request.POST.get("Trip_price")
-    # 添加到数据库
+    # Add to database
     Trip.objects.create(
         tripname=tripname,
         introduction=introduction,
@@ -37,11 +34,13 @@ def manage_addtrips(request):
 
 
 def manage_orders(request):
+    # show all orders
     data_list = Order.objects.all()
     return render(request, "manage_orders.html", {"data_list": data_list})
 
 
 def delete_orders(request):
+    # delete orders
     delete_Order_id = request.GET.get('delete_Order_id')
     Order.objects.filter(Order_id=delete_Order_id).delete()
     return redirect("/manage_orders/")
@@ -52,6 +51,7 @@ def update_orders(request, update_Order_id):
         row_object = models.Order.objects.filter(Order_id=update_Order_id).first()
         # print(row_object.Order_id, row_object.amount, row_object.time, row_object.status, row_object.Order_price)
         return render(request, 'update_orders.html', {"row_object": row_object})
+    # update orders
     amount = request.POST.get("amount")
     models.Order.objects.filter(Order_id=update_Order_id).update(amount=amount)
 
