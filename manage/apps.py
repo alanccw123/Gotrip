5cc2244fc29740ba7f89d44ab1b50b1c2b02d7f5
Ryan <284236428@qq.com>
2023-03-19 16:34:48 +0000

    merge

--- manage/apps.py
+++ manage/apps.py
@@ -1,12 +1,10 @@
 from django.apps import AppConfig
 
-
-<<<<<<<< HEAD:manage/apps.py
 class manageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "manage"
-========
+
 class ChengConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "Trip_review"
->>>>>>>> Dev:Trip_review/apps.py
+
